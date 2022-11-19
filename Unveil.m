4dad3ca94befe1796535280d94022f0c398479c5
D.F <mtac@mtac.app>
2022-11-19 16:11:22 -0500

    Update Unveil.m

--- Unveil.m
+++ Unveil.m
@@ -1,10 +1,15 @@
 #import <UIKit/UIKit.h>
 #import <dlfcn.h>
 
+// https://github.com/opa334/TrollStore/blob/2d762b39effa7c9cd2c5ea8fcd389516a90cd558/RootHelper/main.m#L35
+
 typedef void (*_CFPreferencesSetValueWithContainerType)(CFStringRef key, CFPropertyListRef value, CFStringRef applicationID, CFStringRef userName, CFStringRef hostName, CFStringRef containerPath);
 typedef Boolean (*_CFPreferencesSynchronizeWithContainerType)(CFStringRef applicationID, CFStringRef userName, CFStringRef hostName, CFStringRef containerPath);
 
 void override() {
+	
+	// https://github.com/opa334/TrollStore/blob/2d762b39effa7c9cd2c5ea8fcd389516a90cd558/RootHelper/main.m#L810
+	
 	_CFPreferencesSetValueWithContainerType _CFPreferencesSetValueWithContainer = (_CFPreferencesSetValueWithContainerType)dlsym(RTLD_DEFAULT, "_CFPreferencesSetValueWithContainer");
 	_CFPreferencesSynchronizeWithContainerType _CFPreferencesSynchronizeWithContainer = (_CFPreferencesSynchronizeWithContainerType)dlsym(RTLD_DEFAULT, "_CFPreferencesSynchronizeWithContainer");
 
